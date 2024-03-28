4190ff6f59afa0fe5a19fd961852a4ee9d2428eb
SamSumanth6 <sumanthvenkatesh6@gmail.com>
2024-03-28 08:49:47 +0530

    wallet balance and expenses

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -3,8 +3,16 @@ import './heroSection.css'
 const  HeroSection = ()=>{
     return(
         <div className="main-div">
-            <div className='wallet-div'></div>
-            <div className='expenses-div'></div>
+            <div className='hero-sub-section'>
+                <div className='wallet-div'>
+                    <p className='wallet-subHeading'>Wallet Balance:<span className='money'> ₹4500</span></p>
+                    <button className='wallet-btn'>+Add Income</button>
+                </div>
+                <div className='expenses-div'>
+                    <p className='expenses-subHeading'>Expenses:<span className='E-money'> ₹500</span></p>
+                    <button className='expenses-btn'>+Add Expense</button>
+                </div>
+            </div>
             <div className='chart-div'></div>
         </div>
     )
